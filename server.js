0b09e2cb045886e7de2f3c270a22dedae75e2615
LeeYoHan <yohan4551@gmail.com>
2021-03-27 22:44:35 +0900

    feat: console chat with socket.io

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { resolve, join } from "path";
+import { Server } from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const __dirname = resolve();
@@ -7,6 +9,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => res.render("home"));
@@ -14,4 +17,19 @@ app.get("/", (req, res) => res.render("home"));
 const handleListening = () =>
   console.log(`Server is running: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
